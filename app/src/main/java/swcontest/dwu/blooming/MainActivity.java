ec29bfe303d074fcc429c15af293d2b824907b9c
bae1022 <shbk0423@naver.com>
2021-07-28 16:56:18 +0900

    일상 기록 Intent 추가

--- app/src/main/java/swcontest/dwu/blooming/MainActivity.java
+++ app/src/main/java/swcontest/dwu/blooming/MainActivity.java
@@ -2,7 +2,9 @@ package swcontest.dwu.blooming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClick(View v) {
+        Intent intent = null;
+
+        switch (v.getId()) {
+            case R.id.btn_life: //일상 기록
+                intent = new Intent(this, DailyMemoActivity.class);
+                break;
+
+        }
+        if (intent != null)
+            startActivity(intent);
+
     }
 }
