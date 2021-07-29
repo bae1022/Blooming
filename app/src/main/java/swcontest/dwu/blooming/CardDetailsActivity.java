221f32dbb4f25ad6fcadb753a7e3809182750ab7
sion <aabbcc030421@gamil.com>
2021-07-29 10:48:56 +0900

    card activity add

---
+++ app/src/main/java/swcontest/dwu/blooming/CardDetailsActivity.java
@@ -0,0 +1,14 @@
+package swcontest.dwu.blooming;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CardDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card);
+    }
+}
