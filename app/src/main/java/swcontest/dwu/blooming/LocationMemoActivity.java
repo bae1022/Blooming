ecdfb1be1203c7508112c1d20653d43b5cafd18a
dangerry <chlekdms412@naver.com>
2021-08-01 18:30:47 +0900

    위치기록 버튼 동작

--- app/src/main/java/swcontest/dwu/blooming/LocationMemoActivity.java
+++ app/src/main/java/swcontest/dwu/blooming/LocationMemoActivity.java
@@ -1,14 +1,13 @@
 package swcontest.dwu.blooming;
 
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class LocationMemoActivity extends AppCompatActivity {
 
-    protected  void onCreate(@Nullable Bundle savedInstanceState) {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
     }
