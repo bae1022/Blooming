c24c7bcf37bdeca7e887c4b39aeb18d86419a1a5
sion <aabbcc030421@gamil.com>
2021-08-07 13:21:07 +0900

    sms 수신 권한 추가, 수신 확인 test

--- app/src/main/java/swcontest/dwu/blooming/MainActivity.java
+++ app/src/main/java/swcontest/dwu/blooming/MainActivity.java
@@ -1,8 +1,12 @@
 package swcontest.dwu.blooming;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -16,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkDangerousPermissions();
 
     //일상 기록 Notification 설정_개발하실 때 주석처리 해주세요
 //        Toast.makeText(getApplicationContext(),"Service 시작", Toast.LENGTH_SHORT).show();
@@ -59,4 +65,46 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
 
     }
+
+    //위험 권한 체크
+    //manifest와 java에 둘 다 권한 허가받는 코드를 작성한다.
+    private void checkDangerousPermissions() {
+        String[] permissions = {
+                Manifest.permission.RECEIVE_SMS
+        };
+
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        for (int i = 0; i < permissions.length; i++) {
+            permissionCheck = ContextCompat.checkSelfPermission(this, permissions[i]);
+            if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+                break;
+            }
+        }
+
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "권한 있음", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "권한 없음", Toast.LENGTH_LONG).show();
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0])) {
+                Toast.makeText(this, "권한 설명 필요함.", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, 1);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1) {
+            for (int i = 0; i < permissions.length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, permissions[i] + " 권한이 승인됨.", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, permissions[i] + " 권한이 승인되지 않음.", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
 }
