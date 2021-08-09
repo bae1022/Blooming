9b3cf26db9467eb8df44dd584c0b7337e6ba2d4a
youkyoungJung <yyk834@naver.com>
2021-08-09 10:37:37 +0900

    diaryActivity material CalendarView 적용

--- app/src/main/java/swcontest/dwu/blooming/diaryActivity.java
+++ app/src/main/java/swcontest/dwu/blooming/diaryActivity.java
@@ -1,9 +1,13 @@
 package swcontest.dwu.blooming;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 public class diaryActivity extends AppCompatActivity {
 
@@ -11,6 +15,10 @@ public class diaryActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
+
+        MaterialCalendarView materialCalendarView = findViewById(R.id.calendarView);
+        materialCalendarView.setSelectedDate(CalendarDay.today());
+        materialCalendarView.addDecorator(new EventDecorator(diaryActivity.this));
     }
 
 }
