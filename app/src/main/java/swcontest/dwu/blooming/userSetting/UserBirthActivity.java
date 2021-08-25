ca4f8458f98f3972c5d4d9de4f2a95b849b97d32
yoouyoou <yny026@gmail.com>
2021-08-25 23:36:21 +0900

    [초기화면] Bundle사용&버튼구현

--- app/src/main/java/swcontest/dwu/blooming/userSetting/UserBirthActivity.java
+++ app/src/main/java/swcontest/dwu/blooming/userSetting/UserBirthActivity.java
@@ -4,18 +4,71 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import swcontest.dwu.blooming.R;
 
 public class UserBirthActivity extends Fragment {
+    private int mYear, mMonth, mDay;
+    DatePicker dp_birth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if(savedInstanceState != null && savedInstanceState.get("userName") != null){
+            Toast.makeText(this.getContext(),"값 존재:" + savedInstanceState.get("userName"), Toast.LENGTH_SHORT).show();
+        }
+        Toast.makeText(this.getContext(),"UserBirth액티비티", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this.getContext(), "w" + savedInstanceState.get("userName") , Toast.LENGTH_SHORT).show();
+
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.setting_birth, container, false);
+
+        dp_birth = rootView.findViewById(R.id.datePicker);
+
+        StartActivity activity = (StartActivity) getActivity();
+        Bundle args = activity.bundle;
+        TextView tv_user = rootView.findViewById(R.id.tv_user);
+
+        Calendar calendar = new GregorianCalendar();
+        mYear = calendar.get(Calendar.YEAR);
+        mMonth = calendar.get(Calendar.MONTH);
+        mDay = calendar.get(Calendar.DAY_OF_MONTH);
+        DatePicker dp = rootView.findViewById(R.id.datePicker);
+        dp.init(mYear, mMonth, mDay, mOnDateChangedListener);
+
+        if(args != null){
+            UserData user = (UserData) args.getSerializable("user");
+            tv_user.setText(user.getName());
+            user.setYear(mYear);
+            user.setMonth(mMonth);
+            user.setDay(mDay);
+            args.putSerializable("user", user);
+        }
         return rootView;
     }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Toast.makeText(this.getContext(), "UserBirth액티비티" + dp_birth.getYear(), Toast.LENGTH_SHORT).show();
+        String birth = dp_birth.getYear() + "/" + dp_birth.getMonth() + "/" + dp_birth.getDayOfMonth();
+        outState.putString("userBirth", birth);
+    }
+
+    DatePicker.OnDateChangedListener mOnDateChangedListener = new DatePicker.OnDateChangedListener() {
+        @Override
+        public void onDateChanged(DatePicker datePicker, int y, int m, int d) {
+            mYear = y;
+            mMonth = m;
+            mDay = d;
+        }
+    };
 }
--- app/src/main/java/swcontest/dwu/blooming/userSetting/UserPeriodActivity.java
+++ app/src/main/java/swcontest/dwu/blooming/userSetting/UserPeriodActivity.java
@@ -4,17 +4,40 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import java.lang.reflect.Array;
 
 import swcontest.dwu.blooming.R;
 
 public class UserPeriodActivity extends Fragment {
+    Spinner sp_period;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.setting_period, container, false);
+        Spinner sp = rootView.findViewById(R.id.spinner);
+        Integer[] arr = {10, 20, 30, 40, 50, 60, 70};
+
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this.getContext(),
+                android.R.layout.simple_spinner_item, arr);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        sp.setAdapter(adapter);
+
+        sp_period = rootView.findViewById(R.id.spinner);
+
         return rootView;
     }
 
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Toast.makeText(this.getContext(), "UserPeriod"+ sp_period.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+        outState.putString("userPeriod", sp_period.getSelectedItem().toString());
+    }
 }
